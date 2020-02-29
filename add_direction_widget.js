392e2290a7b67236f1fd4604cb6d11dfe557a17e
Hoang Thanh Binh <51917268+bdt2019unicorn@users.noreply.github.com>
2020-03-01 12:51:27 +1300

    direction authorize test

--- add_direction_widget.js
+++ add_direction_widget.js
@@ -8,7 +8,7 @@ export function AddDirectionsWidget(Directions, view)
             (
                 {
                     view: view,
-                    routeTaskUrl: "https://utility.arcgis.com/usrsvcs/appservices/AVA7HfDc1IGamElH/rest/services/World/Route/NAServer/Route_World/solve"
+                    routeTaskUrl: "https://utility.arcgis.com/usrsvcs/appservices/AVA7HfDc1IGamElH/rest/services/World/Route/NAServer/Route_World/solve?stops=-117.1957,34.0564; -117.184,34.0546"
                 }
             ); 
             view.ui.add(directions, ui_position);
